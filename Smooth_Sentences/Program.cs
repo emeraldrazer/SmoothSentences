3594837f90b61f8229b38d5b43339b1a02a1bac4
Fiji <ekstremistpotato@gmail.com>
2023-05-20 19:56:27 +0200

    upload

---
+++ Smooth_Sentences/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smooth_Sentences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a sentence:");
+                string input = Console.ReadLine();
+
+                string[] words = input.Split(new char[] { ' ', ',', '.', ':', ';', '?', '!', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                bool isChain = true;
+                for (int i = 0; i < words.Length - 1; i++)
+                {
+                    char lastLetter = words[i][words[i].Length - 1];
+                    char firstLetter = words[i + 1][0];
+                    if (char.ToLower(lastLetter) != char.ToLower(firstLetter))
+                    {
+                        isChain = false;
+                        break;
+                    }
+                }
+
+                if (isChain)
+                {
+                    Console.WriteLine("The Sentence is Smooth!");
+                }
+                else
+                {
+                    Console.WriteLine("The sentence is NOT Smooth!");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
